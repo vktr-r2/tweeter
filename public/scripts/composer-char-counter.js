d1b59be1f2201fd77b8e962743a25534cfb9a9da
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-26 01:43:25 -0400

    Add JQuery to update counter DOM object to be dynamic >> works

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,24 @@
 $(document).ready(function() {
 
   //Select and store the first element with the name "text"
-  const textarea = document.getElementsByName("text")[0];
+  const $textarea = document.getElementsByName("text")[0];
+
   //Add keyup event listener 
-  textarea.addEventListener("keyup", function() {
-    //Declare value const and store textarea value >> wrap 'this' with jQuery to create object + use .val() method to get value
-    const value = $(this).val();
-    //Character count will equal value.length
-    const length = value.length;
-    console.log(value.length);
+  $textarea.addEventListener("keyup", function() {
+
+    //Declare value const and store $textarea value >> wrap 'this' with jQuery to create object + use .val() method to get value
+    const $value = $(this).val();
+  
+    //Characters remaining = 140 minus current value length
+    const $charsRemaining = 140 - $value.length;
+    
+    //Select counter element >> $(this).closest traverses to parent element ".new-tweet" >> find ".counter" traverses back down
+    const $counter = $(this).closest(".new-tweet").find(".counter");
+
+    //Target text key of the counter element/obj and assign $charsRemaining to be its value
+    $counter.text($charsRemaining);
+
+
+
   });
 });
