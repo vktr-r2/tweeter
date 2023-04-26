3a11e7e177f9cf50d28279608c40f8157ae8cc85
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-26 01:07:09 -0400

    keyup listener captures characters, logs value.lenth to console

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
-  const textarea = document.getElementsByName("text")[0];
-  // let x = 0  //REMOVE BEFORE PROD//
-  //input listener because it will allow us to see input && counts copy+paste
-  textarea.addEventListener("input", function() {
-    // x ++;    //REMOVE BEFORE PROD//
-    // console.log(x)   //REMOVE BEFORE PROD//
-  });
 
+  //Select and store the first element with the name "text"
+  const textarea = document.getElementsByName("text")[0];
+  //Add keyup event listener 
+  textarea.addEventListener("keyup", function() {
+    //Declare value const and store textarea value >> wrap 'this' with jQuery to create object + use .val() method to get value
+    const value = $(this).val();
+    //Character count will equal value.length
+    const length = value.length;
+    console.log(value.length);
+  });
 });
