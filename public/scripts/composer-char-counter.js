40b3d1853043e5ded02252af5648018c67fa2b0e
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-26 00:36:34 -0400

    Setup input listener on <textarea> element

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,10 @@
-console.log("test, test");
+$(document).ready(function() {
+  const textarea = document.getElementsByName("text")[0];
+  // let x = 0  //REMOVE BEFORE PROD//
+  //input listener because it will allow us to see input && counts copy+paste
+  textarea.addEventListener("input", function() {
+    // x ++;    //REMOVE BEFORE PROD//
+    // console.log(x)   //REMOVE BEFORE PROD//
+  });
+
+});
