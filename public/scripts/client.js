0bcccc09301ecd711fcaed0460c9e137f8a81ad0
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-27 12:27:44 -0400

    Event listener targeting 'submit' form in HTML + serialize method

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -40,19 +40,6 @@ const data = [
   }
 ]
 
-// const tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//   "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//   "created_at": 1461116232227
-// }
-
-
 
 const createTweetElement = (tweetData) => {
   // Extract the necessary data
@@ -94,17 +81,24 @@ const createTweetElement = (tweetData) => {
     }
   }
   
+  $( "#target" ).on( "submit", function( event ) {
+    alert( "Handler for `submit` called." );
+    event.preventDefault();
+  });
+
+
+
+
+
 $(document).ready(function() {
+
+  //Render existing tweets from hardcoded data
   renderTweets(data);
+
+  //Event listener: target #submit-tweet form, listen for submit
+  $('#submit-tweet').submit(function(event) {
+    event.preventDefault(); // prevent submit from refreshing page
+    console.log($(this).serialize());
+  });
 });
 
-
-
-
-// const $tweet = createTweetElement(tweetData);
-
-// $(document).ready(function() {
-// // Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-// });
