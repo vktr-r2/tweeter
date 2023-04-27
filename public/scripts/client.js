81ab9c6e912067ec8f61b4d3d031d70b87152a16
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-27 02:43:55 -0400

    Define and implement renderTweets function >> passes tests

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,17 +5,54 @@
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Vik",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@vktr-r2"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
     },
-  "content": {
-      "text": "Test tweet... random string here"
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Vik",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@vr" },
+    "content": {
+      "text": "Test, test, 123"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+// const tweetData = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//   "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//   "created_at": 1461116232227
+// }
+
+
 
 const createTweetElement = (tweetData) => {
   // Extract the necessary data
@@ -49,13 +86,25 @@ const createTweetElement = (tweetData) => {
   
     return $article;
 
-
-};
-
-const $tweet = createTweetElement(tweetData);
-
+  };
+  
+  const renderTweets = function(tweets) { 
+    for (const tweet of tweets) {
+      $('#tweets-container').append(createTweetElement(tweet));
+    }
+  }
+  
 $(document).ready(function() {
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(data);
 });
+
+
+
+
+// const $tweet = createTweetElement(tweetData);
+
+// $(document).ready(function() {
+// // Test / driver code (temporary)
+// console.log($tweet); // to see what it looks like
+// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+// });
