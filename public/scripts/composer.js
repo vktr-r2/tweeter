631d87df6ac3119e9af71126ae553639a174fca5
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-29 19:56:13 -0400

    Clean up code

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,32 +1,31 @@
 $(document).ready(function () {
-  //Add keyup event listener
-  $("#tweet-text").on("input", function () {
+  //Declare $counter so it can be used across multiple listeners
+  const $counter = $(".new-tweet .counter");
 
+  //TEXT INPUT EVENT LISTENER >> manage character counter
+  $("#tweet-text").on("input", function () {
     //Use 140 minus current length of text input string (this) to calculate charsRemaining
     const $charsRemaining = 140 - $(this).val().length;
-
-    //Select counter element >> .closest() traverses to parent element, find() traverses back down to target
-    const $counter = $(this).closest(".new-tweet").find(".counter");
-
+  
     //Target counter element and change $charsRemaining to be the text appearing with .text()
     $counter.text($charsRemaining);
-
+  
+    //Add/remove red text to character counter
     if ($charsRemaining < 0) {
-      //
       $counter.addClass("over-limit");
     } else {
       $counter.removeClass("over-limit");
     }
   });
 
-  // Reset $charsRemaining to 140 after tweet submit
+  //FORM SUBMIT LISTENER >> reset character count
   $("form").on("submit", function () {
-    const $counter = $(this).closest(".new-tweet").find(".counter");
     $counter.text(140);
   });
 
-  //Listener targeting nav button.  Toggles new-tweet form visibility
+  //NAV BUTTON CLICK LISTENER >> Toggles ".new-tweet" form visibility 
   $(".nav-button").click(function () {
     $(".new-tweet").slideToggle();
   });
 });
+
