9e2ee58ca91245ac2d13542a8d3619fb1c9b23fa
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-29 17:39:34 -0400

    Change character count event listener to listen for input

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,9 +1,6 @@
 $(document).ready(function () {
-  //Select and store the first element with the name "text"
-  const $textarea = document.getElementsByName("text")[0];
-
   //Add keyup event listener
-  $textarea.addEventListener("keyup", function () {
+  $("#tweet-text").on("input", function () {
     //Declare value const and store $textarea value >> wrap 'this' with jQuery to create object + use .val() method to get value
     const $value = $(this).val();
 
